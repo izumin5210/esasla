758b0c2dda5da91093bcfe5baa0436660898862f
izumin5210 <masayuki@izumin.info>
2017-02-10 02:00:17 +0900

    Implement create post

--- app.rb
+++ app.rb
@@ -9,4 +9,32 @@ def esa_client
 end
 
 post '/' do
+  lines = params[:text].split("\n")
+  name, body = lines[0], lines[1..-1].join("\n")
+  category = ENV['ESA_DEFAULT_CATEGORY']
+
+  if name.include?('/')
+    m = name.match(/\A(?<category>.*)\/(?<name>(?!\/).*)\Z/)
+    name = m[:name]
+    category= m[:category]
+  end
+
+  res = esa_client.create_post(
+    name: name,
+    body_md: body,
+    category: category,
+    user: 'esa_bot',
+  )
+
+  msg = {
+    response_type: 'in_channel',
+    text: 'Created new post',
+    attachments: [
+      {
+        title: res.body['full_name'],
+        title_link: res.body['url'],
+      }
+    ]
+  }
+  json msg
 end
