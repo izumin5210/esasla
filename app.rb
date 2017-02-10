9c4fa6f41bc99c30c974939dd01179801b3ea4b0
izumin5210 <masayuki@izumin.info>
2017-02-10 10:59:00 +0900

    Introduce subcommands, `create` and `list`

--- app.rb
+++ app.rb
@@ -8,10 +8,18 @@ def esa_client
   )
 end
 
-post '/' do
-  lines = params[:text].split("\n")
+def default_category
+  @default_category = ENV['ESA_DEFAULT_CATEGORY']
+end
+
+def create_post(args)
+  lines = args.split("\n")
   name, body = lines[0], lines[1..-1].join("\n")
-  category = ENV['ESA_DEFAULT_CATEGORY']
+  category = default_category
+
+  p lines
+  p name
+  p body
 
   if name.include?('/')
     m = name.match(/\A(?<category>.*)\/(?<name>(?!\/).*)\Z/)
@@ -26,15 +34,85 @@ post '/' do
     user: 'esa_bot',
   )
 
-  msg = {
+  # TODO: handle errors
+
+  res.body
+end
+
+def fetch_posts(args)
+  query = args&.empty? ? "wip:true in:#{default_category}" : args
+
+  res = esa_client.posts(q: query)
+
+  # TODO: handle errors
+
+  res.body['posts']
+end
+
+def build_attatchments_from_posts(posts)
+  posts.map { |post| build_attatchment_from_post(post) }
+end
+
+def build_attatchment_from_post(post)
+  { title: post['full_name'],
+    title_link: post['url'],
+  }
+end
+
+def build_usage_message
+  text = <<~USAGE
+    ```
+    Usage:
+
+      /esasla <command> [<args>]
+
+    The commands are:
+
+      create    create new post. the first line is used as a post title.
+      list      fetch posts. if you pass args, they will use as search queries.
+    ```
+  USAGE
+  {
     response_type: 'in_channel',
-    text: 'Created new post',
     attachments: [
       {
-        title: res.body['full_name'],
-        title_link: res.body['url'],
+        color: 'warning',
+        text: text,
+        mrkdwn: true,
       }
-    ]
+    ],
+    mrkdwn: true,
   }
+end
+
+post '/' do
+  text = params[:text]
+  msg = {}
+
+  if text&.empty?
+    msg = build_usage_message
+  else
+    m = text.match(/\A(?<cmd>\S+)\s*(?<args>.*)/m)
+    args = m[:args]
+
+    case m[:cmd]
+    when 'create'
+      post = create_post(args)
+      msg = {
+        response_type: 'in_channel',
+        text: 'Created new post',
+        attachments: build_attatchments_from_posts([post]),
+      }
+    when 'list'
+      posts = fetch_posts(args)
+      msg = {
+        response_type: 'in_channel',
+        attachments: build_attatchments_from_posts(posts),
+      }
+    else
+      msg = build_usage_message
+    end
+  end
+
   json msg
 end
